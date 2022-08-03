05a44a1d10b529a6e0c08e2ec795205430ddee59
JeongWonYoung <nicednjsdud@naver.com>
2022-08-03 23:30:10 +0900

    Update query to crawler
    upadte query create url_info

--- HUGO_SQL/HUGO_DDL.sql
+++ HUGO_SQL/HUGO_DDL.sql
@@ -47,3 +47,44 @@ CREATE TABLE HUGO_COUPON_REPLY (
 
 -- FK 는 나중에 테이블 모두 합친 후 ALTER TABLE 입력 예정
 -- Event Table End
+
+-- 매장 맛집 명소 테이블
+
+-- 매장 크롤용 URL
+
+DROP TABLE URL_INFO CASCADE CONSTRAINTS;
+CREATE TABLE URL_info (
+	urlNO NUMBER(10) PRIMARY KEY,
+	name varchar2(100), 
+	url varchar2(1000)
+);
+
+-- restaurant
+
+DROP TABLE restaurant_info CASCADE CONSTRAINTS;
+CREATE TABLE restaurant_info(
+	rest_idx number(38) PRIMARY KEY, -- pk
+	rest_name varchar2(200) NOT NULL,
+	rest_address varchar2(500),
+	rest_jibunAddress varchar2(300),
+	rest_phone varchar2(100),
+	rest_menu varchar2(100),
+	rest_price varchar2(200),
+	rest_park varchar2(200),
+	rest_open varchar2(200),
+	rest_menu_list varchar2(1000),
+	rest_update_date date DEFAULT sysdate,
+	rest_tag varchar2(1000),
+	rest_starAvg number(10) DEFAULT 0,
+	rest_visitCount number(10) DEFAULT 0,
+	rest_reviewCount number(10) DEFAULT 0,
+	rest_jjim number(10) DEFAULT 0,
+	hotplace_idx number(10) DEFAULT 1
+);
+
+
+
+
+
+
+
